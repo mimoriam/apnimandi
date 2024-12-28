38d29b4fc167879e8b5ba6dee0fdd57ed10f0caa
Mimo <mimoriam420@gmail.com>
2024-12-28 18:20:20 +0500

    feat(app): add splash screen and icon

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,7 @@ const { withNativeWind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+// Add new extension of file:
+config.resolver.assetExts.push('PNG');
 
 module.exports = withNativeWind(config, { input: './global.css' });
