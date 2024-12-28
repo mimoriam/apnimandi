9026ca47d468762b0109e291630b8226a477a033
Mimo <mimoriam420@gmail.com>
2024-12-28 18:04:43 +0500

    feat(app): added statusbar and safearea

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <StatusBar style="auto" />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { Stack } from 'expo-router';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <View>
-      <Stack.Screen options={{ title: 'Home' }} />
+    <SafeAreaView>
+      <Stack.Screen options={{ headerShown: false }} />
       <Text>Hello</Text>
-    </View>
+    </SafeAreaView>
   );
 }
